e95ab93c4a8606dfc33ff51114c835642cca88b7
kracenaz <krisharace.naz@edu.sait.ca>
2026-07-13 20:28:37 -0600

    Added proper stack nav

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,21 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerBackVisible: false,
+        }}
+      />
+      <Stack.Screen
+        name="emp-info"
+        options={{ title: "Employee Information" }}
+      />
+      <Stack.Screen name="sign-in" options={{ title: "Sign In" }} />
+      <Stack.Screen name="sign-up" options={{ title: "Sign Up" }} />
+    </Stack>
+  );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,7 +6,8 @@ export default function Index() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.container}>
-        <Text>Choose one of the following options:</Text>
+        <Text style={styles.title}>Choose one of the following options:</Text>
+        <Text> </Text>
         <Link style={styles.buttons} href="/emp-info">
           Enter Employee Information
         </Link>
@@ -27,7 +28,23 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  title: {
+    fontWeight: "bold",
+    fontSize: 20,
+  },
   buttons: {
-    padding: 25,
+    padding: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
+    color: "white",
+    backgroundColor: "darkblue",
+    justifyContent: "center",
+    marginBottom: 10,
+    borderRadius: 5,
+  },
+  buttonSection: {
+    flex: 1,
+    flexDirection: "row",
+    gap: 10,
   },
 });
